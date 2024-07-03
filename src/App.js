fb776377e211ba68cd08ad757f4e4c04ba439ded
unknown <pawan59c@gmail.com>
2024-07-03 21:08:48 +1000

    Styled content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Content from './Components/Content/Content';
 import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div>
       <Header/>
-
+      <Content/>
     </div>
   );
 }
