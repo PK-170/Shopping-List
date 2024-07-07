a5ac9022420911ea453b0ac45272bf4a6bab6e91
unknown <pawan59c@gmail.com>
2024-07-07 23:26:25 +1000

    added class

--- temp.js
+++ temp.js
@@ -15,6 +15,18 @@ class Person{
     
 }
 
+class Programer extends Person{
+
+constructor(name, height, color, role){
+    super(name, height, color);
+    this.role = role;
+}
+
+}
+
 var obj1 = new Person("Sam",3,"Blue");
 console.log(obj1);
 console.log(obj1.getName());
+let programer = new Programer("tim", 8, "brown", "developer");
+console.log(programer);
+console.log(programer.getName());
