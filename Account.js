492081ec9c99fc2b4f9dc73130ebad6dea9fb7e9
P K <pawan59c@gmail.com>
2024-07-09 23:24:08 +1000

    added class

---
+++ Account.js
@@ -0,0 +1,13 @@
+
+class User{
+
+ constructor(accountNumber, accountName, accountBalance){
+      this.accountNumber = accountNumber;
+      this.accountName = accountName;
+      this.accountBalance = accountBalance;
+ }
+
+
+ 
+
+}
