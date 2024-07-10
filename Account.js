06dbed304f746efe5e0cfda83ebe2f44232aa953
unknown <pawan59c@gmail.com>
2024-07-10 22:06:35 +1000

    added another class method

--- Account.js
+++ Account.js
@@ -10,19 +10,27 @@ class User{
 
 
  displayBalance(){
-    return this.accountBalance;
+    return console.log("Your account balance is  " + (this.accountBalance));
  }
 
  deposit(amount){
    return console.log("Your account balance is  " + (this.accountBalance += amount));
  }
 
+ withdraw(amount){
+  console.log("withdraw start here");
+  if(this.accountBalance >= amount){this.accountBalance -= amount}
+  else{console.log("Sorry you have insufficient funds")}
+ }
+
  
 }
 
 const user1 = new User(1001, "John", 100);
-console.log(user1);
+//console.log(user1);
 user1.deposit(6600);
-console.log(user1.displayBalance());
+user1.withdraw(6600);
+user1.displayBalance();
+
 
 
