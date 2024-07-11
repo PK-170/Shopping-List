6d0fe6be5b3f8e797299fce887f5a4ddb727bedc
P K <pawan59c@gmail.com>
2024-07-11 18:34:53 +1000

    addded child obj

--- obja.js
+++ obja.js
@@ -4,10 +4,16 @@ const User = {
     age: 40,
     region: "NSW",
     get displayName(){console.log(this.nameFirst + " " + this.nameLast);},
-    set setRegion(newRegion){this.region = newRegion}
+    set setRegion(newRegion){this.region = newRegion},
+    laptop: {
+        brand: "Lenovo",
+        ram: 8,
+        storage: 256,
+    }
 }
 
 User.displayName;
 User.setRegion = "QLD";
 User.age = 20;
 console.log(User.region);
+console.log(User.laptop.brand);
