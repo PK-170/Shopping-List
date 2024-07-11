4b21034651cf11dfe79442022cced9f9de28fb96
P K <pawan59c@gmail.com>
2024-07-11 23:15:09 +1000

    added for loop

--- setmap.js
+++ setmap.js
@@ -1,10 +1,15 @@
-const arr = ["Hello", "This", "is", "Array"];
+const arr = ["Hello", "This", "is", "is", "Array"];
 
-const set = new Set(arr);
+//const set = new Set(arr);
+const set = new Set(["a", "b", "d", "k"]);
 
 set.add("a");
 set.add("b");
 set.add("c");
 set.add("d");
 
-console.log( set);
+//console.log( set);
+
+for(item of set){
+    console.log(item);
+}
