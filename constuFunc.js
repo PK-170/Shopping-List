6efad3a075e298d7ae1646288860dec8612376a5
P K <pawan59c@gmail.com>
2024-07-12 22:37:24 +1000

    added logger function

--- constuFunc.js
+++ constuFunc.js
@@ -18,3 +18,16 @@ const user1 = new User("John", "Hunter", 42, "blue");
 const user2 = new User("Ming", "Young", 22, "blue");
 
 nameFunc(user1);
+
+// closures 
+
+const factoryLogger = (type) => {
+
+    function logger(msg){
+       console.log(`${type} - ${msg}`);
+    }
+    return logger;
+}
+
+let errorLogger = factoryLogger("ERROR");
+errorLogger("This is an error logger");
