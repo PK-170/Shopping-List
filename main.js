918022f60c13a99f35aead49c4a60125ae8dcd92
unknown <pawan59c@gmail.com>
2024-07-13 17:39:15 +1000

    interface TS

--- main.js
+++ main.js
@@ -4,3 +4,15 @@
     var c = x + y;
     console.log(c);
 }
+var me = {
+    name: "Ming",
+    age: 22,
+    speak: function (text) {
+        console.log("text");
+    },
+    spend: function (amount) {
+        console.log("I spent", amount);
+        return amount;
+    }
+};
+console.log(me);
--- main.ts
+++ main.ts
@@ -4,3 +4,24 @@ let y: number = 566600;
 let c: number = x + y;
 console.log(c);
 }
+
+interface User{
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+const me: User = {
+    name: "Ming",
+    age: 22,
+    speak(text: string): void {
+        console.log("text");
+    },
+    spend(amount: number): number{
+        console.log("I spent", amount);
+        return amount;
+    }
+};
+
+console.log(me);
