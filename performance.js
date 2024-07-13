a6b0f003b45e7d57648e5051556da21d19b3cfef
unknown <pawan59c@gmail.com>
2024-07-13 11:52:20 +1000

    for loop version

--- performance.js
+++ performance.js
@@ -4,7 +4,24 @@ function sumup(n){
     return (n / 2) * (n + 1);
 }
 
+function sumupFor(n){
+    let total = 0;
+    for (let i=1; i<=n; i++){
+        total += i;
+    }
+    return total;
+}
+
+// expersion function time
 let start = performance.now();
-console.log(sumup(500));
+let result = sumup(400);
 let end = performance.now();
 console.log(end - start);
+//console.log(result);
+
+//for loop time
+let startfor = performance.now();
+let result1 = sumupFor(400);
+let endfor = performance.now();
+console.log(endfor - startfor);
+//console.log(result1);
