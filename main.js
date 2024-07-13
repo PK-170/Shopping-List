38606d7376843ecb75c3ee5dc9942f202b576efc
unknown <pawan59c@gmail.com>
2024-07-13 17:29:49 +1000

    ts file

---
+++ main.js
@@ -0,0 +1,6 @@
+{
+    var x = 100;
+    var y = 566600;
+    var c = x + y;
+    console.log(c);
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+{let x: number = 100;
+let y: number = 566600;
+
+let c: number = x + y;
+console.log(c);
+}
