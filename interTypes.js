0e6272309fc5159b950d0eb54cd99ff42c122be5
unknown <pawan59c@gmail.com>
2024-07-15 14:11:36 +1000

    generic func

--- interTypes.js
+++ interTypes.js
@@ -1,4 +1,11 @@
+//Generics function
 function convertToArray(input) {
     return [input];
 }
 console.log(convertToArray("Test"));
+//generic function
+function getIndexOfAnArrayItem(arr, arrayItem) {
+    return arr.findIndex(function (item) { return item === arrayItem; });
+}
+var a = [22, 66, 77, 99];
+console.log(getIndexOfAnArrayItem(a, 77));
--- interTypes.ts
+++ interTypes.ts
@@ -1,5 +1,15 @@
+//Generics function
 function convertToArray<T>(input: T): T[]{
    return [input];
 }
 
 console.log(convertToArray("Test"));
+
+//generic function
+
+function getIndexOfAnArrayItem<T>(arr: T[], arrayItem: T){
+    return arr.findIndex((item)=> item === arrayItem);
+}
+
+let a = [22, 66, 77, 99];
+console.log(getIndexOfAnArrayItem(a, 77));
