7dff893de4b6006956dcd73a8fe888cec41af090
P K <pawan59c@gmail.com>
2024-07-23 22:44:09 +1000

    algorithm

--- temp1.js
+++ temp1.js
@@ -76,13 +76,19 @@ console.log(a);
 
 function selectionSort(array){
    for(let i=0; i<array.length; i++){
-      let min=1;
+      let min=i;
       for(let j=i+1; j<array.length; j++){
          if(array[j] < array[min]){
             min = j;
          }
       }
+      if(i !== min){
+         let temp = array[i]
+         array[i] = array[min];
+         array[min]= temp;
+      }
    }
+   return array;
 }
 
 let b = selectionSort(ar)
