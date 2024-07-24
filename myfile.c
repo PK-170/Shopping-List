bb4b384cbae7efeba31bc241b46d349f1157a8b8
P K <pawan59c@gmail.com>
2024-07-24 23:11:49 +1000

    c func

--- myfile.c
+++ myfile.c
@@ -5,10 +5,16 @@ void  greet(){
   printf("Welcome to here");
 }
 
+void add(int a, int b){
+    int c = a+b;
+    printf(" This sum is %d \n",c);
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
     greet();
+    add(55555, 888888);
     printf("Hello World");
     return 0;
 }
