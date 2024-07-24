f11ca583aabe1d5755cc47966458b829b89dd0fd
P K <pawan59c@gmail.com>
2024-07-24 23:17:04 +1000

    c func

--- myfile.c
+++ myfile.c
@@ -10,11 +10,17 @@ void add(int a, int b){
     printf(" This sum is %d \n",c);
 }
 
+void multi(int a, int b){
+    int h = a*b;
+    printf("This multiply is %d", h);
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
     greet();
     add(55555, 888888);
     printf("Hello World");
+    multi(7777777, 999);
     return 0;
 }
