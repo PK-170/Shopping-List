9c13a69650644e27a2b923d80d5a29e42df86dba
P K <pawan59c@gmail.com>
2024-07-24 23:02:49 +1000

    c func

--- myfile.c
+++ myfile.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+
+void  greet(){
+  printf("Welcome to here");
+}
+
 int main(int argc, char const *argv[])
 {
     /* code */
+    greet();
     printf("Hello World");
     return 0;
 }
