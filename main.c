2323a5cf0545b129ebf8bae3fe3a2b70c1d82a4f
P K <pawan59c@gmail.com>
2024-07-25 21:26:54 +1000

    dim array

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 int age[] = {21, 29, 44, 55, 66, 44, 99};
 int length = sizeof(age)/sizeof(age[0]);
 
+//two dimenstion array
+int dimArray[3][3] = {{5, 4,95}, {3, 8, 89}};
 
 int main(){
     
     for(int i=0; i<=length; i++){
        printf("%d \n", age[i]);
     }
-
+    
+    printf("%d", dimArray[0][2]);
     return 0;
 }
