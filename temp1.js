85ba36e346a82d852b755979cbeee9bbc1b87031
P K <pawan59c@gmail.com>
2024-07-26 22:50:19 +1000

    new func

--- temp1.js
+++ temp1.js
@@ -127,7 +127,9 @@ function selecAlgo(array){
    for(let i=0; i<array.length; i++){
       let min=i;
       for(let j=i+1; j<array.length; j++){
-         
+         if(array[j] < array[min]){
+            min = j;
+         }
       }
    }
 }
