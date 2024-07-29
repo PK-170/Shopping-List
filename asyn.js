3752148fb5228ebe3d924fa19f97cb35b4e42581
P K <pawan59c@gmail.com>
2024-07-29 22:05:43 +1000

    Promise func

--- asyn.js
+++ asyn.js
@@ -4,4 +4,13 @@ function greet(){
     console.log("calllback func works");
 }
 
-setInterval(()=>{console.log("intervel func")}, 5000)
+// setInterval(()=>{console.log("intervel func")}, 5000);
+
+// Promise
+
+let newPromise = new Promise(function(myResolve, myReject){
+    let x =5;
+
+    if(x ===5){console.log("OK");}
+    else{console.log("ERROR");}
+});
