e2f8a5c517624ac5b5caf0509af7c6d0a64ef091
P K <pawan59c@gmail.com>
2024-07-30 23:53:41 +1000

    await func

--- asyn.js
+++ asyn.js
@@ -65,3 +65,13 @@ async function g5(){
 }
 
 console.log(g5())
+
+//
+function add() {
+    let counter = 0;
+    function plus() {counter += 1;}
+    plus();   
+    return counter;
+  }
+
+  add()
