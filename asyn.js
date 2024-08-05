c64475875945c99ed118d0da86db96f9a6f7be9a
PK-170 <pawan59c@gmail.com>
2024-08-05 20:46:09 +1000

    callbacks

--- asyn.js
+++ asyn.js
@@ -229,3 +229,12 @@ newFunc();
 //In JavaScript, a callback is simply a function that is passed to another function as a parameter and 
 //is invoked or executed inside the other function. 
 
+const greeting = (name) => {
+  console.log('Hello  ' + name);
+}
+
+const processUserName = (callback) => {
+  namea = 'Zihan';
+  callback(namea);
+}
+processUserName(greeting);
