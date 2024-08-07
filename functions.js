ddc3eb4b438bb4f4f5a255da838c3152366b1059
PK-170 <pawan59c@gmail.com>
2024-08-07 22:07:26 +1000

    nested func

--- functions.js
+++ functions.js
@@ -1,15 +1,18 @@
 console.log("New File");
-
+let b = 699;
 // nested function
 function outerFunc(){
     let a = 100;
 
     function innerFunc(){
         console.log(a); 
-        console.log(b);
-         
+        
+        function childFunc(){
+            console.log(b);            
+        }
+        return {childFunc}; 
     }
     return {innerFunc};
 }
 
-outerFunc().innerFunc();
+outerFunc().innerFunc().childFunc();
