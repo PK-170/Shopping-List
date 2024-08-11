2f67c39f87b83024b89fbe04ef6e8848e9dad2b6
PK-170 <pawan59c@gmail.com>
2024-08-11 16:56:43 +1000

    nested func

--- functions.js
+++ functions.js
@@ -64,4 +64,4 @@ var add =   function (a){
      console.log(add(2)(3)(4));
      console.log(add(3)(4)(5)); 
      console.log(add(-3)(4)(5));
-     console.log(add(-3)(4)(5));
+     console.log(add(-555)(4)(5));
