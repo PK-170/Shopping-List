e1420e6fe3f68d641d41836f0cf83519dcb8b366
PK-170 <pawan59c@gmail.com>
2024-08-13 22:07:09 +1000

    event func

--- functions.js
+++ functions.js
@@ -104,5 +104,8 @@ var add =   function (a){
     // event func 
     
 function runWhileLoopForNSeconds(sec){
-    
+    let start = Date.now(), now = start;
+  while (now - start < (sec*1000)) {
+    now = Date.now();
+  }
 }
